c52021c833cb6323ad3afc1bae6a1207985310ee
Darkempire78 <clement.wawszczyk@gmail.com>
2021-06-28 20:54:12 +0200

    update chromedriverpath

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import json
 import platform
 import time
 
-def main(authToken):
+def main(authToken, chromeDriverPath):
     
     # Open chromedriver
-    if platform.system() == "Windows":
-        driver = webdriver.Chrome("chromedriver.exe")
-    else:
-        driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chromeDriverPath)
 
     # Connection
     driver.get("https://www.twitch.tv")
@@ -27,15 +24,12 @@ def main(authToken):
         except:
             pass
         time.sleep(8)
-    
-# time.sleep(2)
-# driver.find_element_by_css_selector("input[id='login-username']").send_keys(login)
-# driver.find_element_by_css_selector("button[data-a-target='passport-login-button']").click()
 
 if __name__ == '__main__':
     # Read config
     with open("config.json", "r", encoding="utf8") as f:
         config = json.load(f)
         authToken = config["authToken"]
+        chromeDriverPath = config["chromeDriverPath"]
         
-    main(authToken)
+    main(authToken, chromeDriverPath)
